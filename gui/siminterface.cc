4e440eec68cf80464b3cd42bee4ecd3f49eaa7e6
Quentin BUATHIER <qbuathier@tetrane.com>
2020-04-23 11:57:36 +0200

    bochs: output text mode debugger to stderr instead of stdout

--- gui/siminterface.cc
+++ gui/siminterface.cc
@@ -845,7 +845,7 @@ void bx_real_sim_c::debug_puts(const char *text)
     sim_to_ci_event(event);
   } else {
     // text mode debugger: just write to console
-    fputs(text, stdout);
+    fputs(text, stderr);
   }
 }
 #endif
